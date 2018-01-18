b5ed0efe7fce177869cb80ff22565db2019a2266
Jang seongyong <seongyong@Jangui-MacBook-Pro.local>
2018-01-18 17:01:04 +0900

    add tiff2pdf project

---
+++ tiff2pdf/src/main/java/com/twosonsoft/pilot/tiff2pdf/Tiff2PDFConverter.java
@@ -0,0 +1,122 @@
+package com.twosonsoft.pilot.tiff2pdf;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.RandomAccessFileOrArray;
+import com.lowagie.text.pdf.codec.TiffImage;
+
+public class Tiff2PDFConverter
+{
+	public boolean isTiffFile(String tiffFilename)
+	{
+		boolean isTiff = false;
+		// Read tiff file header
+		// open file as binary mode
+		// read 2 bytes
+		// if read bytes is 'II' or 'MM' it is TIFF Header
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		try
+		{
+			fis = new FileInputStream(tiffFilename);
+			// 파일의 내용을 문자로 읽기 위해서 InputStreamReader 로 생성
+			isr = new InputStreamReader(fis);
+			// Data의 표시 대상 지정 System.out 에 문자를 출력한다.
+			char[] header = new char[2];
+			isr.read(header, 0, 2);
+
+			String headerMarker = new String(header);
+
+			if ("MM".equals(headerMarker) || "II".equals(headerMarker))
+			{
+				isTiff = true;
+			}
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				fis.close();
+				isr.close();
+			}
+			catch (Exception e)
+			{
+			}
+
+		}
+
+		return isTiff;
+	}
+
+	public void convertTiff2Pdf(String tiffFilename, String pdfFilename) throws Exception
+	{
+		// create pdf document
+		Document pdf = new Document();
+		// set margin 0
+		pdf.setMargins(0, 0, 0, 0);
+
+		PdfWriter.getInstance(pdf, new FileOutputStream(pdfFilename));
+		// open pdf document
+		pdf.open();
+		// add tiff file to pdf document
+		addTiffImageToPdf(pdf, tiffFilename);
+		// close pdf document
+		pdf.close();
+
+	}
+
+	public void convertMultiTiff2Pdf(List<String> files, String pdfFilename) throws Exception
+	{
+		// create pdf document
+		Document pdf = new Document();
+		// set margin 0
+		pdf.setMargins(0, 0, 0, 0);
+
+		PdfWriter.getInstance(pdf, new FileOutputStream(pdfFilename));
+		// open pdf document
+		pdf.open();
+		for (String tiffFilename : files)
+		{
+			// add tiff file to pdf document
+			addTiffImageToPdf(pdf, tiffFilename);
+		}
+		// close pdf document
+		pdf.close();
+
+	}
+
+	void addTiffImageToPdf(Document pdf, String tiffFilename) throws Exception
+	{
+		RandomAccessFileOrArray file = new RandomAccessFileOrArray(tiffFilename);
+		int pages = TiffImage.getNumberOfPages(file);
+		for (int page = 1; page <= pages; page++)
+		{
+			Image img = TiffImage.getTiffImage(file, page);
+			float w = img.getWidth();
+			float h = img.getHeight();
+			// convert horizontal image to vertical
+			if (w > h)
+			{
+				img.setRotationDegrees(90);
+			}
+
+			img.scaleToFit(PageSize.A4.getWidth(), PageSize.A4.getHeight());
+
+			pdf.add(img);
+		}
+
+	}
+
+}
---
+++ tiff2pdf/src/test/java/com/twosonsoft/pilot/tiff2pdf/TiffTest.java
@@ -0,0 +1,29 @@
+package com.twosonsoft.pilot.tiff2pdf;
+
+import org.junit.Test;
+
+public class TiffTest
+{
+	@Test
+	public void testReadTiffHeader()
+	{
+		Tiff2PDFConverter converter = new Tiff2PDFConverter();
+		
+		String tiffFilename = "/Users/seongyong/Downloads/tiffsample";
+		
+		if(converter.isTiffFile(tiffFilename))
+		{
+			System.out.println("It's a tiff file");
+		}
+	}
+	@Test
+	public void testTiff2Pdf() throws Exception
+	{
+		String tiffFilename = "/Users/seongyong/Downloads/multi.tif";
+		String pdfFilename = "/Users/seongyong/Downloads/covertedPdf6.pdf";
+		
+		Tiff2PDFConverter converter = new Tiff2PDFConverter();
+		converter.convertTiff2Pdf(tiffFilename, pdfFilename);
+		
+	}
+}
